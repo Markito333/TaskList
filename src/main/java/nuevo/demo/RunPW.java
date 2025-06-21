1e6ff31fe9f7af12f0361176df35a0f86d89b9fc
Markito333 <markitodiazfaulin@example.com>
2025-06-21 09:41:38 -0400

    Nuevas modificaciones complementarias

--- src/main/java/nuevo/demo/RunPW.java
+++ src/main/java/nuevo/demo/RunPW.java
@@ -2,15 +2,16 @@ package nuevo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
 @SpringBootApplication
-@EnableJpaRepositories("nuevo.demo.repository") // Asegura el escaneo de repositorios
-@EntityScan("nuevo.demo.model") // Escanea las entidades
+@ServletComponentScan
 public class RunPW {
+
     public static void main(String[] args) {
+
         SpringApplication.run(RunPW.class, args);
-    }
-}
+    }  
+
+} 
