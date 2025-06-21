d0ee880d7b62b1e0d67dbb5b6f65e6a9fdc9323a
Markito333 <markitodiazfaulin@example.com>
2025-06-21 08:46:36 -0400

    Nuevas modificaciones

--- src/main/java/nuevo/demo/RunPW.java
+++ src/main/java/nuevo/demo/RunPW.java
@@ -2,7 +2,8 @@ package nuevo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
