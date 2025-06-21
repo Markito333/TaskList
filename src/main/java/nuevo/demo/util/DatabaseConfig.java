82b66a9e013f46274f849c046e5f01452bdb378e
Markito333 <markitodiazfaulin@example.com>
2025-06-21 09:13:29 -0400

    Nuevas modificaciones al config y ap 2

--- src/main/java/nuevo/demo/util/DatabaseConfig.java
+++ src/main/java/nuevo/demo/util/DatabaseConfig.java
@@ -2,21 +2,32 @@ package nuevo.demo.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import javax.sql.DataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 
 @Configuration
 public class DatabaseConfig {
-    
+
     @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-            .url("jdbc:postgresql://" + System.getenv("PGHOST") + ":" + 
-                 System.getenv("PGPORT") + "/" + 
-                 System.getenv("PGDATABASE") + "?sslmode=require")
-            .username(System.getenv("PGUSER"))
-            .password(System.getenv("PGPASSWORD"))
-            .driverClassName("org.postgresql.Driver")
-            .build();
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("nuevo.demo.model");
+        
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
+        
+        em.setJpaVendorAdapter(vendorAdapter);
+        return em;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf.getObject());
+        return transactionManager;
     }
 }
