0adf5191740d08b5cf850d7740c0fb02e1be17d4
Markito333 <markitodiazfaulin@example.com>
2025-06-21 08:56:31 -0400

    Nuevas modificaciones

---
+++ src/main/java/nuevo/demo/util/JpaConfig.java
@@ -0,0 +1,20 @@
+package nuevo.demo.util;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class JpaConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("nuevo.demo.model");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return em;
+    }
+}
