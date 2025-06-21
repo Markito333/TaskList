90469e09a31529027f423dbf36c707db5d15332d
Markito333 <markitodiazfaulin@example.com>
2025-06-21 09:00:03 -0400

    Nuevas modificaciones al jpaConfig

--- src/main/java/nuevo/demo/util/JpaConfig.java
+++ src/main/java/nuevo/demo/util/JpaConfig.java
@@ -1,20 +1,40 @@
 package nuevo.demo.util;
 
 import javax.sql.DataSource;
-
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class JpaConfig {
+    
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+            .driverClassName("org.postgresql.Driver")
+            .url(System.getenv("JDBC_DATABASE_URL")) // O usa las variables individuales
+            .username(System.getenv("PGUSER"))
+            .password(System.getenv("PGPASSWORD"))
+            .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
+        em.setDataSource(dataSource());
         em.setPackagesToScan("nuevo.demo.model");
         em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
     }
 }
